06f3ef59d1857101012e15a4de618dc90ce6c465
James Ellercamp <jellercamp@timeplay.com>
2022-03-31 18:10:42 -0400

    Managed to use props and children with a common layout for each page, up to styling next

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>Hi there! I'm the proud creator of weewee perfume and the rest of this site.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
